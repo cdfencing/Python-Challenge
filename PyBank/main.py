a2425e4ca3cb057fab08c2335ea19300f0d9cc3c
cdfencing <65666202+cdfencing@users.noreply.github.com>
2020-06-18 13:35:27 -0500

    Python Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,7 +7,9 @@ csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 
 print("opening", csvpath)
 
-count_of_months = 0
+count_of_month = 0
+
+
 with open(csvpath) as csvfile:
     print(csvfile)
     # CSV reader specifies delimiter and variable that holds contents
@@ -15,9 +17,30 @@ with open(csvpath) as csvfile:
     print(csvreader)
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print("Financial Analysis ", type(csv_header)) 
-    print(f"CSV Header: {csv_header}")
+    print("Financial Analysis ") 
+    print("_________________________")
 
     for row in csvreader:
-        count_of_months += 1
-count_of_months
+        count_of_month += 1
+    print(f"Total Months: {count_of_month}")
+
+    total = 0
+    for row in csvreader:
+        total += int(row[1])
+    print(f"Total: ${total}")
+    #DEBUG! NEED TO FIGURE OUT HOW TO TOTAL THE COLUMN
+
+    #for row in csvreader:
+        #count_of_month += 1
+    #print(f"Average  Change: {count_of_month}")
+
+    #for row in csvreader:
+        #count_of_month += 1
+    #print(f"Greatest Increase in Profits: {count_of_month}")
+
+    #for row in csvreader:
+        #count_of_month += 1
+    #print(f"Greatest Decrease in Profits: {count_of_month}")
+
+
+
