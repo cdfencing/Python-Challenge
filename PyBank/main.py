a30c6d42358d1c9705b00ec32937b4aac82a21e6
cdfencing <65666202+cdfencing@users.noreply.github.com>
2020-06-19 10:20:24 -0500

    Python Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,26 +8,34 @@ csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 print("opening", csvpath)
 
 count_of_month = 0
-
+total = 0
 
 with open(csvpath) as csvfile:
-    print(csvfile)
+    
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader, None) #skip header
+   
+    total = total + csvreader[1]
+    print(csvfile)
     print(csvreader)
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     print("Financial Analysis ") 
     print("_________________________")
 
-    for row in csvreader:
+    for roiw in csvreader:
         count_of_month += 1
     print(f"Total Months: {count_of_month}")
 
-    total = 0
-    for row in csvreader:
-        total += int(row[1])
+    for i in csvreader:
+        total += int(i[1])
     print(f"Total: ${total}")
+   
+   
+   
+   
+   
     #DEBUG! NEED TO FIGURE OUT HOW TO TOTAL THE COLUMN
 
     #for row in csvreader:
