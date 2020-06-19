b4bb20000781cb7e833dbc1f655184cc0e1989af
cdfencing <65666202+cdfencing@users.noreply.github.com>
2020-06-19 11:40:11 -0500

    Python Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,42 +1,39 @@
 
 import os
-
 import csv
 
 csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
-
 print("opening", csvpath)
-
 count_of_month = 0
 total = 0
 
 with open(csvpath) as csvfile:
-    
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader, None) #skip header
-   
-    total = total + csvreader[1]
-    print(csvfile)
-    print(csvreader)
+            
+    headerrow = next(csvreader) #skip header
+    nextrow = next(csvreader) # grab next row
+    
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    print("Financial Analysis ") 
-    print("_________________________")
-
-    for roiw in csvreader:
+    print("Financial Analysis: ") 
+    print("_ _ _ _ _ _ _ _ _ _ _ _ _")
+    for row in csvreader:
         count_of_month += 1
     print(f"Total Months: {count_of_month}")
-
-    for i in csvreader:
-        total += int(i[1])
+    
+    #grab value from the next row, it is now in a list (grabs second value)
+    total = total + int(nextrow[1])
     print(f"Total: ${total}")
+
+    
+    #print(f"Total Months: {count_of_month}")
+    #for i in csvreader:
+        #total += int(i[1])
+    #print(f"Total: ${total}")
    
    
-   
-   
-   
-    #DEBUG! NEED TO FIGURE OUT HOW TO TOTAL THE COLUMN
+    
 
     #for row in csvreader:
         #count_of_month += 1
