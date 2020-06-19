68868f09d3f3f07f079037b3af964fdfa988b6fd
cdfencing <65666202+cdfencing@users.noreply.github.com>
2020-06-19 12:26:59 -0500

    Python Challenge

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,46 +6,52 @@ csvpath = os.path.join('.', 'Resources', 'budget_data.csv')
 print("opening", csvpath)
 count_of_month = 0
 total = 0
+avg = 0
+max_inc_profit = 0
+max_dec_profit = 0
 
 with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-            
-    headerrow = next(csvreader) #skip header
-    nextrow = next(csvreader) # grab next row
     
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     print("Financial Analysis: ") 
-    print("_ _ _ _ _ _ _ _ _ _ _ _ _")
+    print("------------------------")
     for row in csvreader:
+        #grab value from the next row, 
+        # it is now in a list (grabs second value)
+        total = total + int(row[1])
         count_of_month += 1
-    print(f"Total Months: {count_of_month}")
+        avg = total / len(row[1]) #debug, not right value
+        max_inc_profit = max(row[1]) #debug, not right value
+        max_inc_profit = min(row[1]) #debug, not right value
     
-    #grab value from the next row, it is now in a list (grabs second value)
-    total = total + int(nextrow[1])
+    print(f"Total Months: {count_of_month}")  
     print(f"Total: ${total}")
-
+    print(f"Average Change: {avg}") #debug
+    print(f"Greatest Increase in Profits: {max_inc_profit}") #debug
+    print(f"Greatest Decrease in Profits: {max_dec_profit}") #debug      
     
-    #print(f"Total Months: {count_of_month}")
-    #for i in csvreader:
-        #total += int(i[1])
-    #print(f"Total: ${total}")
-   
+    
+    
+    
+    #headerrow = next(csvreader) #skip header
+    #nextrow = next(csvreader) # grab next row 
    
     
 
     #for row in csvreader:
         #count_of_month += 1
-    #print(f"Average  Change: {count_of_month}")
+    
 
     #for row in csvreader:
         #count_of_month += 1
-    #print(f"Greatest Increase in Profits: {count_of_month}")
+    
 
     #for row in csvreader:
         #count_of_month += 1
-    #print(f"Greatest Decrease in Profits: {count_of_month}")
+    
 
 
 
